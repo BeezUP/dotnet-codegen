77a51d6742c4f97b9cf14e5caf6f151e3f8da9c9
Jêrome Rx <jerome+git@rouaix.eu>
2019-04-29 15:00:01 +0200

    TemplateHelperTests

---
+++ Dotnet.CodeGen.Tests/TemplateHelperTests.cs
@@ -0,0 +1,56 @@
+﻿using Dotnet.CodeGen.CodeGen;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Dotnet.CodeGen.Tests
+{
+    public class TemplateHelperTests
+    {
+        [Fact]
+        public void GetTemplates()
+        {
+            var expecteds = new[]
+            {
+                (path:"_samples/test0\\template.cshtml", fName:"template", dir:""),
+                (path:"_samples/test0\\template2.txt.cshtml", fName:"template2.txt", dir:""),
+                (path:"_samples/test0\\subFolder\\template.cs.cshtml", fName:"template.cs", dir:"subFolder"),
+                (path:"_samples/test0\\subFolder\\template2.cshtml", fName:"template2", dir:"subFolder"),
+                (path:"_samples/test0\\subFolder\\subsub\\temp.cshtml", fName:"temp", dir:"subFolder\\subsub"),
+            };
+
+            var tInfos = TemplateHelper.GetTemplates("_samples/test0", extension: "*.cshtml").ToArray();
+
+            for (int i = 0; i < expecteds.Length; i++)
+            {
+                var info = tInfos[i];
+                var (path, fName, dir) = expecteds[i];
+
+                info.FilePath.ShouldBe(path);
+                info.FileName.ShouldBe(fName);
+                info.Directory.ShouldBe(dir);
+            }
+
+            tInfos.Length.ShouldBe(expecteds.Length);
+        }
+
+        [Fact]
+        public void SingleFileFolder()
+        {
+            var tInfos = TemplateHelper.GetTemplates("_samples/test0/template.cshtml").Single();
+            tInfos.FilePath.ShouldBe("_samples/test0/template.cshtml");
+            tInfos.FileName.ShouldBe("template");
+            tInfos.Directory.ShouldBe("");
+        }
+
+        [Fact]
+        public void EmptyTemplate()
+        {
+            TemplateHelper.GetTemplates("folder/nonExistingfile.cshtml").Count().ShouldBe(0);
+            TemplateHelper.GetTemplates("nonExistingFolder").Count().ShouldBe(0);
+        }
+    }
+}

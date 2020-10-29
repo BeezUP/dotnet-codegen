0e30fa6e3907789066645bfe9cadd5907e74597c
Jérôme Rx <jerome+git@rouaix.eu>
2020-10-29 22:50:19 +0100

    Exception_should_provide_the_helper_name_in_the_message

--- src/CodegenUP.Engine/CustomHandlebars/CodeGenHelperException.cs
+++ src/CodegenUP.Engine/CustomHandlebars/CodeGenHelperException.cs
@@ -8,8 +8,8 @@ namespace CodegenUP.CustomHandlebars
     public class CodeGenHelperException : ArgumentException
     {
         public CodeGenHelperException() { }
-        public CodeGenHelperException(string helperName, string message) : base($"{helperName} : {message}") { }
-        public CodeGenHelperException(string helperName, string message, Exception inner) : base($"{helperName} : {message}", inner) { }
+        public CodeGenHelperException(string helperName, string message) : base($"{helperName}: {message}") { }
+        public CodeGenHelperException(string helperName, string message, Exception inner) : base($"{helperName}: {message}", inner) { }
         protected CodeGenHelperException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context) : base(info, context) { }

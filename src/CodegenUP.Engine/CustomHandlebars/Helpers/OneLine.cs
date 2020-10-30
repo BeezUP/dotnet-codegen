9a27d0b7e5e06c630b3708b304d28805e6bbcdcd
Jérôme Rx <jerome+git@rouaix.eu>
2020-10-30 22:46:50 +0100

    better one liner

--- src/CodegenUP.Engine/CustomHandlebars/Helpers/OneLine.cs
+++ src/CodegenUP.Engine/CustomHandlebars/Helpers/OneLine.cs
@@ -1,4 +1,5 @@
 ﻿using HandlebarsDotNet;
+using System;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -10,31 +11,33 @@ namespace CodegenUP.CustomHandlebars.Helpers
     /// Append a single line, taking out empty lines & meaningless whitespaces from the inner template
     /// </summary>
 #if DEBUG
-    [HandlebarsHelperSpecification("{}", "{{#one_line}} {{/one_line}}", "")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}} |do not < remove please >| {{/one_line}}", "|do not < remove please >|")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}} \n {{/one_line}}", "")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}\n {{/one_line}}", "")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}\n{{/one_line}}", "")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}} \r\n {{/one_line}}", "")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}\r\n{{/one_line}}", "")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}} test{{/one_line}}", "test")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}} a \n z {{/one_line}}", "az")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}a\n z{{/one_line}}", "az")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}a\nz{{/one_line}}", "az")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}a \r\n z{{/one_line}}", "az")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}a \r\n \r\n \r\nz{{/one_line}}", "az")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}test\r\n\r\n\r\ntest{{/one_line}}", "testtest")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}{{/one_line}}", "")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line}}   test {{/one_line}}", "test")]
-    [HandlebarsHelperSpecification("{}", "{{#one_line 5}}test{{/one_line}}", "     test")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}} {{/one_line}}", "\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}} |do not < remove please >| {{/one_line}}", "|do not < remove please >|\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}} \n {{/one_line}}", "\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}\n {{/one_line}}", "\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}\n{{/one_line}}", "\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}} \r\n {{/one_line}}", "\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}\r\n{{/one_line}}", "\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}} test{{/one_line}}", "test\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}} a \n z {{/one_line}}", "az\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}a\n z{{/one_line}}", "az\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}a\nz{{/one_line}}", "az\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}a \r\n z{{/one_line}}", "az\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}a \r\n \r\n \r\nz{{/one_line}}", "az\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}test\r\n\r\n\r\ntest{{/one_line}}", "testtest\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line 0 'true'}}test\r\n\r\n\r\ntest{{/one_line}}", "testtest\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line 0 'false'}}test\r\n\r\n\r\ntest{{/one_line}}", "testtest")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}{{/one_line}}", "\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line}}   test {{/one_line}}", "test\n")]
+    [HandlebarsHelperSpecification("{}", "{{#one_line 5}}test{{/one_line}}", "     test\n")]
 #endif
-    public class OneLine : SimpleBlockHelperBase<object, int?>
+    public class OneLine : SimpleBlockHelperBase<object, int?, bool?>
     {
         static readonly Regex regex = new Regex(@" *[\r\n]+ *", RegexOptions.Compiled);
 
         public OneLine() : base("one_line") { }
 
-        public override void HelperFunction(TextWriter output, HelperOptions options, object context, int? indent, object[] otherArguments)
+        public override void HelperFunction(TextWriter output, HelperOptions options, object context, int? indent, bool? lineBreak, object[] otherArguments)
         {
             EnsureArgumentsCountMax(otherArguments, 0);
 
@@ -48,7 +51,11 @@ namespace CodegenUP.CustomHandlebars.Helpers
             using var tr = new StreamReader(stream);
             var result = tr.ReadToEnd();
             result = regex.Replace(result, "");
-            result = new string(' ', indent ?? 0) + result.Trim();
+            result = 
+                new string(' ', indent ?? 0) 
+                + result.Trim() 
+                + (lineBreak ?? true ? "\n" : "")
+                ;
             output.WriteSafeString(result);
         }
     }
